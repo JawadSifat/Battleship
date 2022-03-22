299e2969cac17f48a4e40f1e702f34f70a1ce675
ssavio30 <101643133+ssavio30@users.noreply.github.com>
2022-03-21 21:49:07 -0400

    Update BattleShipGame.java

--- BattleShipGame.java
+++ BattleShipGame.java
@@ -1,46 +1,42 @@
-import java.util.Scanner;
-
 public class BattleShipGame {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		while(true){
-			
-			Ocean battlefield = new Ocean();
-			
-			// set up the game
-			battlefield.placeAllShipsRandomly();
-			battlefield.print();
-			
-			while(!battlefield.isGameOver()) {
-				// accept "shots" from the user; 
-				
-				System.out.print("Enter row: ");
-				int row = sc.readInt();
-				System.out.print("Enter column: ");
-				int column = sc.readInt();
-				
-				//display the results; 
-				battlefield.print();
-				if(battlefield.shootAt(row, column)){
-					System.out.println("You just sank a " + battlefield.getShipArray[row][column].getShipType());
-				}
-			}
-			
-			//print final scores; 
-			System.out.println("# of shots fired: ");
-			System.out.println(battlefield.getShotsFired());
-			System.out.println("Hit count: ");
-			System.out.println(battlefield.getHitCount());
-			
-			// and ask the user if he/she wants to play again
-			System.out.println("play again? (true/false)");
-			boolean again = sc.readBoolean();
-			if (!again) break;
-		}
-		
-		sc.close();
-	}
+
+    public static void main(String[] args) {
+
+        while (true) {
+
+            Ocean battlefield = new Ocean();
+
+            // set up the game
+            battlefield.placeAllShipsRandomly();
+            battlefield.print();
+
+            while (!battlefield.isGameOver()) {
+                // accept "shots" from the user;
+
+                System.out.print("Enter row: ");
+                int row = StdIn.readInt();
+                System.out.print("Enter column: ");
+                int column = StdIn.readInt();
+
+                //display the results;
+                battlefield.print();
+                if (battlefield.shootAt(row, column)) {
+                    System.out.println("You just sank a " + battlefield.getShipArray()[row][column]
+                            .getShipType());
+                }
+            }
+
+            //print final scores;
+            System.out.println("# of shots fired: ");
+            System.out.println(battlefield.getShotsFired());
+            System.out.println("Hit count: ");
+            System.out.println(battlefield.getHitCount());
+
+            // and ask the user if he/she wants to play again
+            System.out.println("play again? (true/false)");
+            boolean again = StdIn.readBoolean();
+            if (!again) break;
+        }
+
+    }
 }
